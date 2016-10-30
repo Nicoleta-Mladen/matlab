97553712fb8d36c8ffee8d40039b0e8ce9a6fba3
Nicoleta-Mladen <mladennicoleta05@yahoo.com>
2016-10-30 20:41:44 +0200

    Add files via upload

---
+++ exercitiul 1.m
@@ -0,0 +1,31 @@
+   % a)
+a=0:0.1:2;     %generarea vectorului a
+lungimea= length(a)  %se determina lungimea vectorului a, care trebuie sa aiba aceeasi lungime cu vectorul b pentru a se putea realiza inmultirea celor doi vectori
+b=ones (21,1);  % dupa ce am determinat lungimea vectorului a se genereaza vectorul b
+a*b            %se inmultesc cei doi vectori, a  si b 
+  % b) 
+b*a            %se face inmultirea lui b cu a
+  % c) 
+%se realizeaza inmultirea element cu element a celor doi vectori
+a(1)*b(1)
+a(2)*b(2)
+a(3)*b(3)
+a(4)*b(4)
+a(5)*b(5)
+a(6)*b(6)
+a(7)*b(7)
+a(8)*b(8)
+a(9)*b(9)
+a(10)*b(10)
+a(11)*b(11)
+a(12)*b(12)
+a(13)*b(13)
+a(14)*b(14)
+a(15)*b(15)
+a(16)*b(16)
+a(17)*b(17)
+a(18)*b(18)
+a(19)*b(19)
+a(20)*b(20)
+a(21)*b(21)
+%in urma calculelor se obtine vectorul a 
